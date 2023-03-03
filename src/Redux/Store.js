090da614940721080e23bc0c8643e307d076c25d
VitaliyLenin <vitaliylenin77@gmail.com>
2023-03-03 22:47:46 +0200

    Files update

---
+++ src/Redux/Store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore } from 'redux-persist';
+import persistedReducer from './root-reduser';
+
+// import rootReducer from './root-reduser';
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
--- src/components/Contacts/MyContacts.jsx
+++ src/components/Contacts/MyContacts.jsx
@@ -1,22 +1,22 @@
-import { nanoid } from 'nanoid';
-import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import MyContactForm from 'components/MyContactForm/MyContactForm';
 import MyContactList from 'components/ContactList/ContactList';
 import MyContactsFilter from 'components/MyContactsFilter/MyContactsFilter';
 
+import { addContact, removeContact } from 'Redux/Contacts/contacts-slice';
+import { setFilter } from 'Redux/Filter/filter-slice';
+
+import { getFilteredContacts } from 'Redux/Contacts/contacts-selectors';
+import { getFilter } from 'Redux/Filter/filter-selectors';
+
 import css from './MyContacts.module.css';
 
 const MyContacts = () => {
-  const [contacts, setContacts] = useState(() => {
-    const contacts = JSON.parse(localStorage.getItem('my-contacts'));
-    return contacts ? contacts : [];
-  });
-  const [filter, setFilter] = useState('');
+  const contacts = useSelector(getFilteredContacts);
+  const filter = useSelector(getFilter);
 
-  useEffect(() => {
-    localStorage.setItem('my-contacts', JSON.stringify(contacts));
-  }, [contacts]);
+  const dispatch = useDispatch();
 
   const isDublicate = name => {
     const normalizedName = name.toLowerCase();
@@ -27,151 +27,31 @@ const MyContacts = () => {
     return Boolean(result);
   };
 
-  const addContact = ({ name, number }) => {
+  const handleAddContact = ({ name, number }) => {
     if (isDublicate(name)) {
       return alert(`${name} is already in contacts`);
     }
 
-    setContacts(prevContacts => {
-      const newContact = {
-        id: nanoid(),
-        name,
-        number,
-      };
-
-      return [newContact, ...prevContacts];
-    });
+    dispatch(addContact({ name, number }));
   };
 
-  const removeContact = id => {
-    setContacts(prevContacts =>
-      prevContacts.filter(contact => contact.id !== id)
-    );
+  const handleRemoveContact = id => {
+    dispatch(removeContact(id));
   };
 
   const handleFilter = ({ target }) => {
-    setFilter(target.value);
+    dispatch(setFilter(target.value));
   };
-
-  const getFilteredContacts = () => {
-    if (!filter) {
-      return contacts;
-    }
-
-    const normalizedName = filter.toLowerCase();
-    const result = contacts.filter(({ name }) => {
-      return name.toLowerCase().includes(normalizedName);
-    });
-
-    return result;
-  };
-
-  const filteredContacts = getFilteredContacts();
 
   return (
     <div>
       <h2 className={css.title}>Phonebook</h2>
-      <MyContactForm onSubmit={addContact} />
+      <MyContactForm onSubmit={handleAddContact} />
       <MyContactsFilter handleChange={handleFilter} value={filter} />
       <h2 className={css.title}>Contacts</h2>
-      <MyContactList
-        removeContact={removeContact}
-        contacts={filteredContacts}
-      />
+      <MyContactList removeContact={handleRemoveContact} contacts={contacts} />
     </div>
   );
 };
 
 export default MyContacts;
-
-// class MyContacts extends Component {
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   };
-
-//   componentDidMount() {
-//     const contacts = JSON.parse(localStorage.getItem('my-contacts'));
-//     if (contacts && contacts.length) {
-//       this.setState({ contacts });
-//     }
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     const { contacts } = this.state;
-//     if (prevState.contacts.length !== contacts.length) {
-//       localStorage.setItem('my-contacts', JSON.stringify(contacts));
-//     }
-//   }
-
-//   addContact = ({ name, number }) => {
-//     if (this.isDublicate(name)) {
-//       return alert(`${name} is already in contacts`);
-//     }
-
-//     this.setState(prevState => {
-//       const { contacts } = prevState;
-//       const newContact = {
-//         id: nanoid(),
-//         name,
-//         number,
-//       };
-
-//       return { contacts: [newContact, ...contacts], name: '', number: '' };
-//     });
-//   };
-
-//   handleFilter = ({ target }) => {
-//     this.setState({ filter: target.value });
-//   };
-
-//   removeContact = id => {
-//     this.setState(({ contacts }) => {
-//       const newContact = contacts.filter(contact => contact.id !== id);
-//       return { contacts: newContact };
-//     });
-//   };
-
-// isDublicate(name) {
-//   const normalizedName = name.toLowerCase();
-//   const { contacts } = this.state;
-//   const result = contacts.find(({ name }) => {
-//     return name.toLowerCase() === normalizedName;
-//   });
-
-//     return Boolean(result);
-//   }
-
-//   getFilteredContacts() {
-//     const { filter, contacts } = this.state;
-
-//     if (!filter) {
-//       return contacts;
-//     }
-
-//     const normalizedName = filter.toLowerCase();
-//     const result = contacts.filter(({ name }) => {
-//       return name.toLowerCase().includes(normalizedName);
-//     });
-
-//     return result;
-//   }
-
-//   render() {
-//     const { addContact, removeContact, handleFilter } = this;
-//     const contacts = this.getFilteredContacts();
-
-//     return (
-//       <div>
-//         <h2 className={css.title}>Phonebook</h2>
-//         <MyContactForm onSubmit={addContact} />
-//         <MyContactsFilter
-//           handleChange={handleFilter}
-//           value={this.state.filter}
-//         />
-//         <h2 className={css.title}>Contacts</h2>
-//         <MyContactList removeContact={removeContact} contacts={contacts} />
-//       </div>
-//     );
-//   }
-// }
